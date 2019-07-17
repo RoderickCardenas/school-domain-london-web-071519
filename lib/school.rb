38202e08225c975abfc80a5d2166359a7ee7580f
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-17 10:27:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,4 +8,10 @@ class School
   def roster
     @roster = {}
   end
+  
+  add_student(student_name, grade)
+    @student_name = student_name
+    @grade = grade
+    @roster[grade] = student_name
+  end
 end
