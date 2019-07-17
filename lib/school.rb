3ce5501ba7e975da5c5a94fc4e2191d7e8a24f87
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-17 10:48:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class School
   attr_accessor :name, :roster
   
@@ -16,6 +18,7 @@ class School
   end
   
   def sort
-    @roster.sort[0]
+    @roster.sort
+    binding.pry
   end
 end
